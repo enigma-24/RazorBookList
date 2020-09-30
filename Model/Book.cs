0a8a4ffed11772eded101f3bab364dfa258d8369
Aniket <aniket.d2402@gmail.com>
2020-09-29 22:05:57 -0400

    Add Model for Book

---
+++ Model/Book.cs
@@ -0,0 +1,15 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorBookList.Model
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+    }
+}
