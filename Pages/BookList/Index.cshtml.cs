e1908f18f856cfb29757b6f7985efdee8794f86d
Aniket <aniket.d2402@gmail.com>
2020-10-01 18:36:13 -0400

    Added Delete Functionality

--- Pages/BookList/Index.cshtml.cs
+++ Pages/BookList/Index.cshtml.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using RazorBookList.Data;
@@ -18,6 +19,16 @@ namespace RazorBookList.Pages.BookList
         public async Task OnGet()
         {
             Books = await context.Book.ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostDelete(int id){
+            var bookToDelete = await context.Book.FindAsync(id);
+            if(bookToDelete != null){
+                context.Book.Remove(bookToDelete);
+                await context.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return NotFound();
         }
     }
 }
